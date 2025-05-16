f3f66a4c07990f87d3b91eb7eb4e067f6b7c4387
Keven <kevendicamargo@gmail.com>
2025-05-16 18:49:10 -0300

    feat: adicionando os componentes a pagina home e aplicando o estilo reset

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,38 +1,119 @@
 import Header from "../../components/Header";
+import Banner from "../../components/Banner";
+import CardProduct from "../../components/CardProduct";
+import Button from "../../components/Button";
+import Timer from "../../components/Timer";
+import Footer from "../../components/Footer";
+
+import "./styles/main.sass";
+
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Home = () => {
   return (
     <>
       <Header />
+
       <section className="banners">
-        <div className="banner" id="banner-1">
-          <div className="banner-cover"></div>
-          <div className="banner-content">
-            <h2>Promoção de Notebooks</h2>
-            <a href="#">
-              Ver Mais <i className="fas fa-arrow-circle-right"></i>
-            </a>
-          </div>
-        </div>
-        <div className="banner" id="banner-2">
-          <div className="banner-cover"></div>
-          <div className="banner-content">
-            <h2>Lançamento de Acessórios</h2>
-            <a href="#">
-              Ver Mais <i className="fas fa-arrow-circle-right"></i>
-            </a>
-          </div>
-        </div>
-        <div className="banner" id="banner-3">
-          <div className="banner-cover"></div>
-          <div className="banner-content">
-            <h2>Câmeras Potentes</h2>
-            <a href="#">
-              Ver Mais <i className="fas fa-arrow-circle-right"></i>
-            </a>
-          </div>
+        <Banner id="banner-1" text="Promoção de Notebooks" />
+        <Banner id="banner-2" text="Lançamento de Acessórios" />
+        <Banner id="banner-3" text="Câmeras Potentes" />
+      </section>
+
+      <section className="products-grid">
+        <h2>Produtos Novos</h2>
+        <div className="products-grid-container">
+          <CardProduct
+            preco="124,90"
+            categoria="categoria"
+            nome="Nome Produto"
+            tag="new"
+          />
+          <CardProduct
+            preco="124,90"
+            categoria="categoria"
+            nome="Nome Produto"
+            tag="new"
+          />
+          <CardProduct
+            preco="124,90"
+            categoria="categoria"
+            nome="Nome Produto"
+            tag="new"
+          />
+          <CardProduct
+            preco="124,90"
+            categoria="categoria"
+            nome="Nome Produto"
+            tag="new"
+          />
         </div>
       </section>
+
+      <section className="deal-container">
+        <div className="deal-container-content">
+          <Timer />
+          <h2 className="deal-container-content-title">
+            Promoção de Fone Gamer
+          </h2>
+          <p className="deal-container-content-subtitle">
+            Toda a linha gamer com 50% de desconto
+          </p>
+          <button className="btn">Comprar Agora</button>
+        </div>
+      </section>
+
+      <section className="products-grid">
+        <h2>Mais Vendidos</h2>
+        <div className="products-grid-container">
+          <CardProduct
+            preco="124,90"
+            categoria="categoria"
+            nome="Nome Produto"
+            tag="hot"
+          />
+          <CardProduct
+            preco="124,90"
+            categoria="categoria"
+            nome="Nome Produto"
+            tag="hot"
+          />
+          <CardProduct
+            preco="124,90"
+            categoria="categoria"
+            nome="Nome Produto"
+            tag="hot"
+          />
+          <CardProduct
+            preco="124,90"
+            categoria="categoria"
+            nome="Nome Produto"
+            tag="hot"
+          />
+        </div>
+      </section>
+
+      <section className="newsletter-container">
+        <h2>
+          Assine a nossa <span>Newsletter</span>
+        </h2>
+        <form action="">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Digite o seu e-mail"
+          />
+          <Button type="submit" style="btn btn-half" text="Assinar" />
+        </form>
+        <div className="social-media">
+          <FaFacebookF className="icon" />
+          <FaInstagram className="icon" />
+          <FaTwitter className="icon" />
+        </div>
+      </section>
+
+      <Footer />
     </>
   );
 };
