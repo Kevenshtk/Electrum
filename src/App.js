809306a22a8317f6598516457855e6bbf9a7bdf8
Keven <kevendicamargo@gmail.com>
2025-08-14 12:45:28 -0300

    feat: adicionar aviso inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/home";
-import ProductsRegister from "./pages/register/products";
-import ListProducts from "./pages/listProducts";
-import "./styles/reset.sass";
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/home';
+import ProductsRegister from './pages/register/products';
+import ListProducts from './pages/listProducts';
+
+import Swal from 'sweetalert2';
+
+import './styles/reset.sass';
 
 function App() {
-  const [currentUser , setCurrentUser ] = useState({
+  const [currentUser, setCurrentUser] = useState({
     status: false,
     email: '',
-    name: ''
+    name: '',
+  });
+
+  Swal.fire({
+    title: 'Projeto em Construção',
+    text: 'funcionalidades como produtos favoritos, carrinho de compras e pesquisar produtos pelo campo de busca não estão disponíveis e bugs podem ser encontrados.',
+    icon: 'warning',
   });
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home currentUser={currentUser} setCurrentUser={setCurrentUser}/>} />
+        <Route
+          path="/"
+          element={
+            <Home currentUser={currentUser} setCurrentUser={setCurrentUser} />
+          }
+        />
         <Route path="/register/products" element={<ProductsRegister />} />
-        <Route path="/list/:category" element={<ListProducts currentUser={currentUser}/>} />
+        <Route
+          path="/list/:category"
+          element={<ListProducts currentUser={currentUser} />}
+        />
       </Routes>
     </Router>
   );
