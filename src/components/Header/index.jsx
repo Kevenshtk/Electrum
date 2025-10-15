bc7dfe8fe7533401cbc9d29904421defdf4f07e8
Keven <kevendicamargo@gmail.com>
2025-10-15 17:09:53 -0300

    refactor: dividir o Header em componentes menores

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,17 @@
 import { useContext } from 'react';
-import {
-  FaPhone,
-  FaRegEnvelope,
-  FaLocationDot,
-  FaDollarSign,
-  FaUser,
-  FaBolt,
-  FaHeart,
-  FaCartShopping,
-} from 'react-icons/fa6';
 import { useNavigate } from 'react-router-dom';
 
 import { AuthContext } from '../../context/auth';
-import useWindowWidth from '../../hooks/useWindowWidth';
-import Button from '../Button';
+
+import HeaderTop from './top';
+import HeaderMain from './main';
+import HeaderBottom from './bottom';
+
 
 import './styles.sass';
 
 const Header = ({ setShowModal, setIsFormRegister }) => {
   const navigate = useNavigate();
-  const width = useWindowWidth();
   const { currentUser } = useContext(AuthContext);
 
   const handleCategoryClick = (category) => {
@@ -34,102 +26,9 @@ const Header = ({ setShowModal, setIsFormRegister }) => {
 
   return (
     <header>
-      <div className="header-top">
-        <div className="header-top-container">
-          <div className="store-info-container">
-            <span>
-              <FaPhone className="icon" />
-              (48) 99999-0000
-            </span>
-            <span>
-              <FaRegEnvelope className="icon" />
-              electrum@email.com
-            </span>
-            <span>
-              <FaLocationDot className="icon" />
-              Rua Teste, 1234
-            </span>
-          </div>
-          <div className="header-user-top-menu">
-            <span>
-              <a href="/">
-                <FaDollarSign className="icon" />
-                BRL
-              </a>
-            </span>
-
-            <FaUser className="icon" />
-            <Button
-              className="btn-sign-in"
-              text={currentUser.status ? currentUser.name : 'Login'}
-              onClick={() => !currentUser.status && handleShowModal('login')}
-            />
-            {!currentUser.status && (
-              <Button
-                className="btn-register"
-                text=" Registrar"
-                onClick={() => handleShowModal('register')}
-              />
-            )}
-          </div>
-        </div>
-      </div>
-
-      <div className="main-header">
-        <a href="/" id="brand">
-          Electrum
-          <FaBolt className="icon" />
-        </a>
-        <form action="" id="search-form">
-          <input type="text" id="search" placeholder="Busque aqui" />
-          <Button type="submit" className="btn btn-half" text="Pesquisar" />
-        </form>
-        <div className="header-actions-menu">
-          <div className="wishlist-container">
-            <span className="qty">0</span>
-            <FaHeart className="icon" />
-            <a href="/">Favoritos</a>
-          </div>
-          <div className="header-cart-container">
-            <span className="qty">0</span>
-            <FaCartShopping className="icon" />
-            <a href="/">Carrinho</a>
-          </div>
-        </div>
-      </div>
-
-      <div className="header-bottom">
-        <nav>
-          <ul>
-            {[
-              'Periféricos',
-              'Pc Gamer',
-              'Hardware',
-              'Notebooks',
-              'Smartphones',
-              'Câmeras',
-              'Acessórios',
-            ].map((category, index) => {
-              if (
-                width <= 435 &&
-                (category === 'Periféricos' ||
-                  category === 'Notebooks' ||
-                  category === 'Câmeras')
-              )
-                return null;
-              return (
-                <li key={index}>
-                  <Button
-                    className="btn-category"
-                    text={category}
-                    onClick={() => handleCategoryClick(category)}
-                  />
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      </div>
+      <HeaderTop currentUser={currentUser} handleShowModal={handleShowModal}/>
+      <HeaderMain />
+      <HeaderBottom handleCategoryClick={handleCategoryClick}/>
     </header>
   );
 };
---
+++ src/components/Header/main/index.jsx
@@ -0,0 +1,44 @@
+import {
+  FaBolt,
+  FaHeart,
+  FaCartShopping,
+} from 'react-icons/fa6';
+import { useNavigate } from 'react-router-dom';
+import Button from '../../Button';
+
+import './styles.sass';
+
+const HeaderMain = () => {
+    const navigate = useNavigate();
+
+    const handleClickHome = () => {
+        navigate('/');
+    }
+
+  return (
+    <div className="main-header">
+      <a id="brand" onClick={handleClickHome}>
+        Electrum
+        <FaBolt className="icon" />
+      </a>
+      <form action="" id="search-form">
+        <input type="text" id="search" placeholder="Busque aqui" />
+        <Button type="submit" className="btn btn-half" text="Pesquisar" />
+      </form>
+      <div className="header-actions-menu">
+        <div className="wishlist-container">
+          <span className="qty">0</span>
+          <FaHeart className="icon" />
+          <a href="/">Favoritos</a>
+        </div>
+        <div className="header-cart-container">
+          <span className="qty">0</span>
+          <FaCartShopping className="icon" />
+          <a href="/">Carrinho</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderMain;
