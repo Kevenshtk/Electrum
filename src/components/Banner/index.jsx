c59d6d5575823be30eb2e1df771f6bbf6507818a
Keven <kevendicamargo@gmail.com>
2025-11-04 20:11:15 -0300

    refactor: substituir botão com onClick por Link do react-router-dom
    no componente Banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,22 +1,15 @@
-import { useNavigate } from 'react-router-dom';
-
-import Button from '../Button';
+import { Link } from 'react-router-dom';
 
 import "./styles.sass";
 
 const Banner = ({ id, text, category }) => {
-  const navegate = useNavigate();
-
-  const handleCategoryClick = (category) => {
-    navegate(`/list/${category}`);
-  }
 
   return (
     <div className="banner" id={id}>
       <div className="banner-cover"></div>
       <div className="banner-content">
         <h2>{text}</h2>
-        <Button className="btn-banner" text="Ver Mais" onClick={() => handleCategoryClick(category)}/>
+        <Link className='link' to={`/list/${category}`}>Ver Mais</Link>
       </div>
     </div>
   );
