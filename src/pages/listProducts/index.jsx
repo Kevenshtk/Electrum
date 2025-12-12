724391859cea9ddb7573ee4803b7543bca582708
Keven <kevendicamargo@gmail.com>
2025-12-11 22:48:41 -0300

    style: atualizar estilo da página de listagem de produtos

--- src/pages/listProducts/index.jsx
+++ src/pages/listProducts/index.jsx
@@ -40,45 +40,51 @@ const ListProducts = () => {
     setVeiwProducts(productsFiltered);
   }, [products, category, selectedTag]);
 
-  const orderProductsByPrice = useCallback((order) => {
-    let productsOrdered = order.includes('Menor')
-      ? [...viewProducts].sort((a, b) => a.price - b.price)
-      : [...viewProducts].sort((a, b) => b.price - a.price);
+  const orderProductsByPrice = useCallback(
+    (order) => {
+      let productsOrdered = order.includes('menor')
+        ? [...viewProducts].sort((a, b) => a.price - b.price)
+        : [...viewProducts].sort((a, b) => b.price - a.price);
 
-    setVeiwProducts(productsOrdered);
-  }, [viewProducts]);
+      setVeiwProducts(productsOrdered);
+    },
+    [viewProducts]
+  );
 
   return (
-    <>
-      <div className="container">
-        <AsideFilterProducts
-          title={formatCategory(category)}
-          totalResult={viewProducts.length}
-          onSelectedTag={setSelectedTag}
-          onOrderProducts={orderProductsByPrice}
-        />
-        <main className="container-list">
-          {viewProducts.length !== 0 ? (
-            viewProducts.map(({ id, tag, image, category, name, price }) => {
-              return (
-                <CardVertical
-                  key={id}
-                  idProduct={id}
-                  className="list"
-                  tag={tag}
-                  image={image}
-                  category={category}
-                  name={name}
-                  price={price}
-                />
-              );
-            })
-          ) : (
-            <p>Nenhum produto encontrado para {category}.</p>
-          )}
-        </main>
+    <div className="products-content">
+      <div className="products-container">
+        <div className="products-layout">
+          <AsideFilterProducts
+            title={formatCategory(category)}
+            totalResult={viewProducts.length}
+            onSelectedTag={setSelectedTag}
+            onOrderProducts={orderProductsByPrice}
+          />
+
+          <main className="products-grid">
+            {viewProducts.length !== 0 ? (
+              viewProducts.map(({ id, tag, image, category, name, price }) => {
+                return (
+                  <CardVertical
+                    key={id}
+                    idProduct={id}
+                    className="list"
+                    tag={tag}
+                    image={image}
+                    category={category}
+                    name={name}
+                    price={price}
+                  />
+                );
+              })
+            ) : (
+              <p>Nenhum produto encontrado para {category}.</p>
+            )}
+          </main>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
