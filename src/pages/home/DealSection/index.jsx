bc72edafeabdc280f2f53dbb56d903ce44522d51
Keven <kevendicamargo@gmail.com>
2026-06-12 16:18:36 -0300

    refactor: seção deal
    - limpar o jsx de DealSection criando state para end date
    - reduzir repetição de código no componente Timer com um map

--- src/pages/home/DealSection/index.jsx
+++ src/pages/home/DealSection/index.jsx
@@ -1,24 +1,32 @@
+import { useState } from 'react';
+
 import Timer from '../../../components/Timer';
+import Button from '../../../components/Button';
 import alert from '../../../utils/alert.js';
 
 import './styles.sass';
 
 const DealSection = () => {
+  const [endDate] = useState(() =>
+    new Date(
+      Date.now() + ((2 * 24 + 12) * 60 * 60 + 25 * 60 + 33) * 1000
+    ).toISOString()
+  );
+
   return (
     <section className="deal-container">
       <div className="deal-container-content">
-        <Timer
-          endDate={new Date(
-            Date.now() + ((2 * 24 + 12) * 60 * 60 + 25 * 60 + 33) * 1000
-          ).toISOString()}
-        />
+        <Timer endDate={endDate} />
+
         <h2 className="deal-container-content-title">Promoção de Fone Gamer</h2>
         <p className="deal-container-content-subtitle">
           Toda a linha gamer com 50% de desconto
         </p>
-        <button className="btn" onClick={alert.unavailable}>
-          Comprar Agora
-        </button>
+        <Button
+          className="btn"
+          text="Comprar Agora"
+          onClick={alert.unavailable}
+        />
       </div>
     </section>
   );
